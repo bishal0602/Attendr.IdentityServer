f0b21ac0f4ce2f7fd1236b38f1fe2ef4e937fbdf
bishal0602 <bishalpanta60@gmail.com>
2023-05-31 12:15:31 +0545

    Updated README

--- Controllers/VerificationController.cs
+++ Controllers/VerificationController.cs
@@ -18,6 +18,16 @@ namespace Attendr.IdentityServer.Controllers
         [HttpGet]
         public async Task<IActionResult> VerifyAccount([FromQuery] string user, [FromQuery] string verify)
         {
+            if (string.IsNullOrWhiteSpace(user))
+            {
+                return BadRequest($"Query paramter '{nameof(user)}' cannot be null or whitespace.");
+            }
+
+            if (string.IsNullOrWhiteSpace(verify))
+            {
+                return BadRequest($"Query parameter '{nameof(verify)}' cannot be null or whitespace.");
+            }
+
             VerificationStatusCodes verificationStatus = await _userRepository.VerifyUserAsync(user, verify);
             return GenerateVerificationResponse(verificationStatus);
         }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-﻿Identity server for Attendr application implementing OAuth 2.0 Resource Owner Password Credential(ROPC) flow
+﻿# Attendr.IdentityServer
 
-Under construction🚧
+## Overview
+
+ASP.NET Core Identity server for Attendr application implementing **OAuth 2.0 Resource Owner Password Credential(ROPC) flow** specifically designed for the Attendr app. Attendr _(now discontinued)_ was an mobile app intended to allow class representatives (CRs) take attendance of students efficiently.
+
+## Features
+
+- User Registration
+- Email Verification
+- Role Management
